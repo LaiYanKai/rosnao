be1986889ee1b739b4670f7a6d0a91ff93f5509d
Lai Yan Kai <lai.yankai@gmail.com>
2023-03-07 22:16:32 +0800

    230307

--- src/rosnao_bridge/scripts/image_relay.sh
+++ src/rosnao_bridge/scripts/image_relay.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 SCRIPT_DIR=$(dirname `echo $(realpath "$0")`)
-source `echo SCRIPT_DIR`/get_bin_dir.sh
+source `echo $SCRIPT_DIR`/get_bin_dir.sh
 
 # Launches the Video Device proxy in Nao, and puts the image into shared memory
-(cd `echo $BIN_DIR`; chmod +x `echo image_pub`; ./image_pub $1 $2 $3 $4) #IP, SHM_ID, RES, CAM
+(cd `echo $BIN_DIR`; chmod +x image_pub; ./image_pub $1 $2 $3 $4) #IP, SHM_ID, RES, CAM
 exit 0
